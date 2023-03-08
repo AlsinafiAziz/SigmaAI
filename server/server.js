0293464c376697c88d7c929e000ca60da1af1a4c
Alsinafi <azizalsinafi@gmail.com>
2023-03-08 10:19:33 +0100

    21 Try

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
  })
+const cors = require('cors')
 
 app.use(cors());
 
